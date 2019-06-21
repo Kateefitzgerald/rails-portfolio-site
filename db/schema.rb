7ace7990df0b667a07f76c6083fca8b26f8ccbf9
Kate Earls FitzGerald <kate.earls.fitzgerald@gmail.com>
2019-06-21 12:36:40 +0100

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_12_161117) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contact_forms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
